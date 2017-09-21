c96f309ec771112d5345da6f1217c6b100d685b7
Sean Handley <sean.handley@gmail.com>
2017-09-21 14:50:25 +0100

    Separate test code from library code.

--- client.go
+++ client.go
@@ -1,35 +1,41 @@
-package main
+package datacentred
 
 import (
-  "bytes"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
-  "fmt"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func Request(verb string, path string) {
-  client := &http.Client{}
-
-  req, _ := http.NewRequest(verb, path, nil)
-  req.Header.Add("Accept", "application/vnd.datacentred.api+json; version=1")
-  req.Header.Add("Authorization", "Token token=foo:bar")
-  req.Header.Add("Content-Type", "application/json")
-
-  resp, _ := client.Do(req)
-  s, _    := ioutil.ReadAll(resp.Body)
-  resp.Body.Close()
-
-  var prettyJSON bytes.Buffer
-  error := json.Indent(&prettyJSON, s, "", "  ")
-  if error != nil {
-      fmt.Println("JSON parse error: ", error)
-  }
-
-  fmt.Println((string(prettyJSON.Bytes())))
-  return
+func PrettyPrintJson(input []byte) {
+	var prettyJSON bytes.Buffer
+	err := json.Indent(&prettyJSON, input, "", "  ")
+	if err != nil {
+		fmt.Println("JSON parse error: ", err)
+	} else {
+		fmt.Println(string(prettyJSON.Bytes()))
+	}
 }
 
-func main() {
-  Request("GET", "https://my.datacentred.io/api")
+func Request(verb string, path string) ([]byte, error) {
+	client := &http.Client{}
+
+	var url = "https://my.datacentred.io/api/" + path
+
+	req, _ := http.NewRequest(verb, url, nil)
+	req.Header.Add("Accept", "application/vnd.datacentred.api+json; version=1")
+	req.Header.Add("Authorization", "Token token=foo:bar")
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp_body, nil
 }
---
+++ test_datacentred.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "datacentred"
+  "fmt"
+)
+
+func main() {
+  data, err := datacentred.Request("GET", "users")
+  if err != nil {
+    fmt.Errorf("Request failed: %s", err)
+  } else {
+    datacentred.PrettyPrintJson(data)
+  }
+}

44d7303288ce11b9d80b7ff07eb2f8d569dfc5c6
Sean Handley <sean.handley@gmail.com>
2017-09-22 20:20:27 +0100

    Finish the usage structs.

--- usage.go
+++ usage.go
@@ -27,38 +27,90 @@ type ProjectImagesUsage struct {
 	Usage        []UsageBreakdown
 }
 
+type InstanceHistoryEntry struct {
+	Billable   bool
+	EventName  string `json:"event_name"`
+	Flavor     string
+	RecordedAt string `json:"recorded_at"`
+	Seconds    int
+	State      string
+	UserId     string `json:"user_id"`
+}
+
+type InstanceFlavor struct {
+	Id         string
+	Name       string
+	RamMb      int `json:"ram_mb"`
+	RootDiskGb int `json:"root_disk_gb"`
+	VcpusCount int `json:vcpus_count"`
+}
+
 type ProjectInstancesUsage struct {
-	CreatedAt string `json:"created_at"`
-	DeletedAt string `json:"deleted_at"`
-	Id        string
-	Name      string
+	FirstBootedAt string `json:"first_booted_at"`
+	TerminatedAt  string `json:"terminated_at"`
+	Id            string
+	Name          string
+	LatestState   string `json:"latest_state"`
+	History       []InstanceHistoryEntry
+	Tags          []string
+	CurrentFlavor InstanceFlavor `json:"current_flavor"`
+	Usage         []UsageBreakdown
+}
+
+type IpQuotaChange struct {
+	Previous   int
+	Quota      int
+	RecordedAt string `json:"recorded_at"`
 }
 
 type ProjectIpsUsage struct {
+	CurrentQuota int `json:"current_quota"`
+	QuotaChanges []IpQuotaChange
+	Usage        []UsageBreakdown
 }
 
 type ProjectLoadBalancersUsage struct {
+	Id           string
+	Name         string
+	Owner        string
+	StartedAt    string `json:"started_at"`
+	TerminatedAt string `json:"terminated_at"`
+	Usage        []UsageBreakdown
 }
 
 type ProjectVolumesUsage struct {
+	CreatedAt    string `json:"created_at"`
+	DeletedAt    string `json:"deleted_at"`
+	Id           string
+	LatestSizeGb int `json:"latest_size_gb"`
+	Name         string
+	Owner        string
+	Tags         []string
+	Usage        []UsageBreakdown
 }
 
 type ProjectVpnsUsage struct {
+	Id           string
+	Name         string
+	Owner        string
+	StartedAt    string `json:"started_at"`
+	TerminatedAt string `json:"terminated_at"`
+	Usage        []UsageBreakdown
 }
 
 type ProjectUsage struct {
 	Id    string
 	Name  string
 	Usage struct {
-		Images []ProjectImagesUsage
-		// Instance []ProjectInstancesUsage
-		// Ips []ProjectIpsUsage
-		// LoadBalancers []ProjectLoadBalancersUsage `json:"load_balancers"`
-		// ObjectStorage struct {
-		//   Usage []UsageBreakdown
-		// } `json:"object_storage"`
-		// Volumes []ProjectVolumesUsage
-		// Vpns []ProjectVpnsUsage
+		Images        []ProjectImagesUsage
+		Instances     []ProjectInstancesUsage
+		Ips           ProjectIpsUsage
+		LoadBalancers []ProjectLoadBalancersUsage `json:"load_balancers"`
+		ObjectStorage struct {
+			Usage []UsageBreakdown
+		} `json:"object_storage"`
+		Volumes []ProjectVolumesUsage
+		Vpns    []ProjectVpnsUsage
 	}
 }
 

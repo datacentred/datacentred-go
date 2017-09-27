b1aeb8fa3f4ac999bd4e354baa0ccc2f0c7451ab
Sean Handley <sean.handley@gmail.com>
2017-09-27 10:52:43 +0100

    More doc.

--- request.go
+++ request.go
@@ -10,33 +10,43 @@ import (
 	"os"
 )
 
+// ApiError contains details of a single error returned from the API.
 type ApiError struct {
 	Detail   string
 	Field    string
 	Resource string
 }
 
-type ApiErrors struct {
+type apiErrorsResponse struct {
 	Errors []ApiError
 }
 
+// ApiVersion is the API microversion this SDK currently targets on server.
 const ApiVersion = "1"
+// ContentType is the request content type for the API.
 const ContentType = "application/json"
+// AcceptType is the response type we accept from the server.
 const AcceptType = "application/vnd.datacentred.api+json"
+// BaseUri is the server's base URI before path suffixes are added.
 const BaseUri = "https://my.datacentred.io/api/"
 
+// Configuration is a structure for config data this SDK.
 type Configuration struct {
 	Client    http.Client
 	AccessKey string
 	SecretKey string
 }
 
+// Config holds config information for this SDK.
+// This includes API credentials and a HTTP transport client.
 var Config = Configuration{
 	Client:    http.Client{},
 	AccessKey: os.Getenv("DATACENTRED_ACCESS"),
 	SecretKey: os.Getenv("DATACENTRED_SECRET"),
 }
 
+// Request makes a single HTTP request against the API server and returns
+// the server's response as a byte sequence.
 func Request(verb string, path string, body []byte) ([]byte, error) {
 	var url = BaseUri + path
 
@@ -67,7 +77,7 @@ func Request(verb string, path string, body []byte) ([]byte, error) {
 		return nil, errors.New("Not found")
 	case
 		422:
-		var apiErrors ApiErrors
+		var apiErrors apiErrorsResponse
 		json.Unmarshal(resp_body, &apiErrors)
 		fmt.Println(apiErrors.Errors[0].Detail)
 		return nil, errors.New(apiErrors.Errors[0].Detail)

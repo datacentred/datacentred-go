c2b38a6553227d48b435eb58036d34dddebf1940
Sean Handley <sean.handley@gmail.com>
2017-10-02 10:53:01 +0100

    Test misc server errors.

---
+++ request_test.go
@@ -0,0 +1,15 @@
+package datacentred
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestMiscServerErrors(t *testing.T) {
+  r := initRecorder("fixtures/server_error")
+  defer r.Stop()
+
+  _, err := Request("POST", "roles", nil)
+
+  assert.Equal(t, "Server returned 400 Bad Request.", err.Error(), "they should be the same")
+}
